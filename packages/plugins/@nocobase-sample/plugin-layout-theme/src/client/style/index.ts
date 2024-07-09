5de02834f48f87f8d074910621f93e4207781d8e
肖兴(xiaox) <349560971@qq.com>
2024-07-09 17:09:23 +0800

    style: 取消createGlobal注释

--- packages/plugins/@nocobase-sample/plugin-layout-theme/src/client/style/index.ts
+++ packages/plugins/@nocobase-sample/plugin-layout-theme/src/client/style/index.ts
@@ -2,9 +2,8 @@ import '../style/layout-content';
 import '../style/layout-header';
 
 export default function createGlobalStyle(key: string, css: string) {
-  // if (document.body.querySelector(`style[name="${key}"]`)) {
-  //   return;
-  // }
-
-  // document.body.insertAdjacentHTML('beforeend', `<style name="${key}">${css}</style>`);
+  if (document.body.querySelector(`style[name="${key}"]`)) {
+    return;
+  }
+  document.body.insertAdjacentHTML('beforeend', `<style name="${key}">${css}</style>`);
 }
